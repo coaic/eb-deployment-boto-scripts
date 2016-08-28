f79eabaa42ea37fffd5b4c045580d209df041226
Shane MacPhillamy <rcoaic@gmail.com>
2016-08-28 12:45:34 +1000

    Checkpoint of JSON

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # eb-deployment-boto-scripts
 Suite of orchestration scripts to create VPC, EFS File System, CloudFront Distribution and Elastic Beanstalk
+
+The scripts make several assumptions about the AWS environment:
+
+1.  Python3 is installed
+
+2.  Assumes all scripts are in current directory.
+
+3.  Assumes the region is us-west-2
+
+4.  Assumes the .war file to be launched is in the same folder as the scripts and is named ROOT.war
+
+5.  Assumes an email address for Simple Notification Service notifications: rcoaic at gmail dot com
+
+6.  The AWS CLI must be installed. Refer to: http://docs.aws.amazon.com/cli/latest/userguide/installing.html
+
+7.  Assumes aws configure set preview.cloudfront true command has been run so that CloudFront can be configured.
+
+8.  The aws configure command must be run to store/configure an API key/secret that can be accessed via commands.
+
+9.  No API keys or account credentials are stored in the scripts or repository.
+
+10.  The development environment is currently Mac OS command line.
+
+11. Various packages not provided via Mac OS have been installed with Homebrew. Refer to http://brew.sh
+
+12. There is a reliance on AWS Elastic Beanstalk to create an initial bucket to maintain configuration data needed by
+    the service in the region being used.
+
+13. Assumes an EC2 key pair exists for the region, assumes shelde-test-us-west-2.
+
+14. Assumes default roles have been created by Elastic Beanstalk named: aws-elasticbeanstalk-ec2-role and aws-elasticbeanstalk-service-role
+    -- this is a one time requirement, roles are not region specific.
+    -- aws-elasticbeanstalk-ec2-role should have the attached policies { AWSElasticBeanstalkWebTier, CloudWatchLogsFullAccess,
+                                                                         AWSElasticBeanstalkMulticontainerDocker, AWSElasticBeanstalkWorkerTier }
+    -- aws-elasticbeanstalk-service-role should have attached policies { AWSElasticBeanstalkEnhancedHealth, AWSElasticBeanstalkService }
+
+15. The EC2s launched by Elastic Beanstalk have public IPs assigned -- public internet connectivity is required for the EC2s to access
+    Elastic Beanstalk API end points.
+
+16. An alternative would be to modify the VPC creation script to create a NAT Service in a public subnet to allow instances with only
+    private IPs to have AWS API access to the NAT service.
+
+17. If you want to rerun ./create_beanstalk_with_eb_api.py from console, please ensure you terminate the environment followed by deleting the
+    application in the AWS Console.
+
+18. Prior to deployment the .war file is unpacked to ./war, the .ebextensions folder in the root of the .war is removed and seeded with configured
+    .ebextensions folder to allow configuring launched Ec2s with access to the Elastic File System File at /efs on the EC2. The static assets are
+    moved to a folder ./assets to be coppied to an S3 bucket prefix.
+
+
--- create_cloudfront_distribution.sh
+++ create_cloudfront_distribution.sh
@@ -3,13 +3,15 @@
 # Create CloudFront distribution using an S3 bucket as an orgin
 #
 region="us-west-2"
-bucket_name_prefix="shelde-test-distribution"
-distribution_name="blue"
 account_id=$(aws ec2 describe-security-groups --group-names 'Default' --query 'SecurityGroups[0].OwnerId' --output text --region ${region})
+bucket_name_prefix="shelde-test-distribution"
+distribution_domain="${bucket_name_prefix}-${region}-${account_id}.s3.amazonaws.com"
+distribution_prefix="/blue"
 #
 # Files created/overwritten by script
 #
 bucket_policy="bucket-policy.json"
+distribution_config="cloud-front-distribution.json"
 
 local_distribution_content="cloudfront"
 
@@ -47,7 +49,201 @@ EOF
     echo "Waiting for bucket eventual consistency..."
     sleep 60
     aws s3api put-bucket-policy --bucket ${bucket_name} --policy file://$(pwd)/${bucket_policy}
+    #
+    # Configure the CloudFront distribution
+    #
 
 fi
 
-aws s3 sync ${local_distribution_content} "s3://${bucket_name}/${distribution_name}/" --delete
+    cat <<EOF >${distribution_config}
+{
+    "DistributionConfig": {
+        "CallerReference": "",
+        "Aliases": {
+            "Quantity": 0,
+            "Items": [
+                ""
+            ]
+        },
+        "DefaultRootObject": "",
+        "Origins": {
+            "Quantity": 0,
+            "Items": [
+                {
+                    "Id": "S3-${distribution_domain}",
+                    "DomainName": "${distribution_domain}",
+                    "OriginPath": "${distribution_prefix}",
+                    "CustomHeaders": {
+                        "Quantity": 0,
+                        "Items": [
+                            {
+                                "HeaderName": "",
+                                "HeaderValue": ""
+                            }
+                        ]
+                    },
+                    "S3OriginConfig": {
+                        "OriginAccessIdentity": ""
+                    },
+                    "CustomOriginConfig": {
+                        "HTTPPort": 80,
+                        "HTTPSPort": 443,
+                        "OriginProtocolPolicy": "http-only",
+                        "OriginSslProtocols": {
+                            "Quantity": 0,
+                            "Items": [
+                                ""
+                            ]
+                        }
+                    }
+                }
+            ]
+        },
+        "DefaultCacheBehavior": {
+            "TargetOriginId": "S3-${distribution_domain}",
+            "ForwardedValues": {
+                "QueryString": false,
+                "Cookies": {
+                    "Forward": "none",
+                    "WhitelistedNames": {
+                        "Quantity": 0,
+                        "Items": [
+                            ""
+                        ]
+                    }
+                },
+                "Headers": {
+                    "Quantity": 0,
+                    "Items": [
+                        ""
+                    ]
+                }
+            },
+            "TrustedSigners": {
+                "Enabled": false,
+                "Quantity": 0,
+                "Items": [
+                    ""
+                ]
+            },
+            "ViewerProtocolPolicy": "allow-all",
+            "MinTTL": 0,
+            "AllowedMethods": {
+                "Quantity": 2,
+                "Items": [
+                    "GET",
+                    "HEAD"
+                ],
+                "CachedMethods": {
+                    "Quantity": 2,
+                    "Items": [
+                        "GET",
+                        "HEAD"
+                    ]
+                }
+            },
+            "SmoothStreaming": false,
+            "DefaultTTL": 0,
+            "MaxTTL": 0,
+            "Compress": true
+        },
+        "CacheBehaviors": {
+            "Quantity": 0,
+            "Items": [
+                {
+                    "PathPattern": "",
+                    "TargetOriginId": "",
+                    "ForwardedValues": {
+                        "QueryString": true,
+                        "Cookies": {
+                            "Forward": "",
+                            "WhitelistedNames": {
+                                "Quantity": 0,
+                                "Items": [
+                                    ""
+                                ]
+                            }
+                        },
+                        "Headers": {
+                            "Quantity": 0,
+                            "Items": [
+                                ""
+                            ]
+                        }
+                    },
+                    "TrustedSigners": {
+                        "Enabled": true,
+                        "Quantity": 0,
+                        "Items": [
+                            ""
+                        ]
+                    },
+                    "ViewerProtocolPolicy": "",
+                    "MinTTL": 0,
+                    "AllowedMethods": {
+                        "Quantity": 0,
+                        "Items": [
+                            ""
+                        ],
+                        "CachedMethods": {
+                            "Quantity": 0,
+                            "Items": [
+                                ""
+                            ]
+                        }
+                    },
+                    "SmoothStreaming": true,
+                    "DefaultTTL": 0,
+                    "MaxTTL": 0,
+                    "Compress": true
+                }
+            ]
+        },
+        "CustomErrorResponses": {
+            "Quantity": 0,
+            "Items": [
+                {
+                    "ErrorCode": 0,
+                    "ResponsePagePath": "",
+                    "ResponseCode": "",
+                    "ErrorCachingMinTTL": 0
+                }
+            ]
+        },
+        "Comment": "",
+        "Logging": {
+            "Enabled": true,
+            "IncludeCookies": true,
+            "Bucket": "",
+            "Prefix": ""
+        },
+        "PriceClass": "",
+        "Enabled": true,
+        "ViewerCertificate": {
+            "CloudFrontDefaultCertificate": true,
+            "IAMCertificateId": "",
+            "ACMCertificateArn": "",
+            "SSLSupportMethod": "",
+            "MinimumProtocolVersion": "",
+            "Certificate": "",
+            "CertificateSource": ""
+        },
+        "Restrictions": {
+            "GeoRestriction": {
+                "RestrictionType": "none",
+                "Quantity": 0,
+                "Items": [
+                    ""
+                ]
+            }
+        },
+        "WebACLId": ""
+    }
+}
+EOF
+    set -x
+    aws cloudfront create-distribution --distribution-config file://$(pwd)/${distribution_config}
+    #echo $(cat ${distribution_config})
+
+
+# aws s3 sync ${local_distribution_content} "s3://${bucket_name}/${distribution_name}/" --delete
